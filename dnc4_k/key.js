c46e347c5f1c9c48f8054d84884d78b02eb040cc
Jeongwoo Choi <lanslote@hotmail.com>
2025-11-14 16:48:08 +0900

    s

---
+++ dnc4_k/key.js
@@ -0,0 +1,4 @@
+// 던전앤코파일럿4 키코드
+//var KEYCODES = { 1: "help", 2: "easy", 3: "connect", 4: "safe", 5: "new", 6: "create", 7: "power" };
+var KEYCODES = { 1: "111", 2: "222", 3: "333", 4: "444", 5: "555", 6: "666", 7: "777" };
+//var KEYCODES = { 1: "111"};
